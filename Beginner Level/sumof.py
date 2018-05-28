09574cc5d2282a31a3a4dbd36bcae1f15310e514
Geetha5578mary <36623765+Geetha5578mary@users.noreply.github.com>
2018-05-28 16:08:03 +0530

    Create sumof.py

---
+++ Beginner Level/sumof.py
@@ -0,0 +1,10 @@
+def main():
+    count = 0
+    num = int(input(""))
+    while (num > 0):
+      number = num//10
+      count = count + 1
+    print (count)
+
+if __name__ == '__main__':
+    main()
